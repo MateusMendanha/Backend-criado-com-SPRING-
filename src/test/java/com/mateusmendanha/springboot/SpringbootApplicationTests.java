94cffd8734216f5f647b842f991f98afad7a19c6
Mateus M <mateusmendanha@gmail.com>
2020-08-08 16:13:09 -0300

    Inicio do Projeto

---
+++ src/test/java/com/mateusmendanha/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusmendanha.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
